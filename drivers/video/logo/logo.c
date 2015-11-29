164ac177a5b92bfd01f546b327dc74a214151129
gouwa <geekbox@geekbuying.com>
2015-11-29 23:08:59 +0800

    Logo: Geekbox

--- drivers/video/logo/logo.c
+++ drivers/video/logo/logo.c
@@ -78,6 +78,10 @@ const struct linux_logo * __init_refok fb_find_logo(int depth)
 	}
 	
 	if (depth >= 8) {
+#ifdef CONFIG_LOGO_GEEKBOX_CLUT224
+		/* Generic Linux logo */
+		logo = &logo_geekbox_clut224;
+#endif
 #ifdef CONFIG_LOGO_LINUX_CLUT224
 		/* Generic Linux logo */
 		logo = &logo_linux_clut224;
